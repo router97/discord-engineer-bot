cfb1bbce623a9ddcb825dfce2e1495c31d145be6
router <you@example.com>
2024-05-19 02:41:48 +0300

    translation

--- cogs/Translation.py
+++ cogs/Translation.py
@@ -1,5 +1,4 @@
-import json
-import os
+import datetime
 
 import discord
 from discord import app_commands
@@ -7,7 +6,7 @@ from discord.ext import commands
 import langcodes
 
 from bot import translator, bot
-from utilities import ensure_user_is_in_database
+from utilities import ensure_user_is_in_database, ensure_user_is_in_database_interaction
 
 class Translation(commands.Cog):
     """Translation description."""
@@ -21,39 +20,66 @@ class Translation(commands.Cog):
         translate_to = await ctx.bot.db.fetchval('SELECT translate_to FROM users WHERE user_id = $1', ctx.author.id)
         translate_to = langcodes.standardize_tag(translate_to)
         
-        embed = discord.Embed(color=discord.Color.red(), description=translator.translate(message, dest=translate_to).text)
-        
-        await ctx.reply(embed=embed, ephemeral=True)
+        translated_text = translator.translate(text=message, dest=translate_to, src='auto')
+    
+        embed = discord.Embed(
+            color=discord.Color.red(), 
+            title=f'Translating from {langcodes.get(translated_text.src).display_name()} to {langcodes.get(translated_text.dest).display_name()}', 
+            description=f'*{translated_text.text}*', 
+        )
+    
+        await ctx.reply(embed=embed, ephemeral=True, silent=True, delete_after=60.0)
     
     @commands.hybrid_command(name="setlang", description="Set a language to translate to.")
     @ensure_user_is_in_database()
     async def setlang(self, ctx: commands.Context, language: str = commands.parameter(default="en", description="The language you wish to translate to.", displayed_default="English", displayed_name="Language")):
         """Set a language to translate to."""
-        
         try:
-            lang_obj = langcodes.Language.get(language.casefold())
-            if not lang_obj.is_valid():
+            new_language = langcodes.get(language.casefold())
+            if not new_language.is_valid():
                 raise ValueError("Invalid language code or name")
-            language_code = lang_obj.to_alpha3()
+            language_code = new_language.to_alpha3()
         except ValueError:
-            await ctx.reply(f'Invalid language: {language}.', ephemeral=True)
+            embed = discord.Embed(
+                title="Error!",
+                description=f'**Invalid language:** *{language}*',
+                color=discord.Color.red(), 
+            )
+            await ctx.reply(embed=embed, delete_after=60.0, ephemeral=True, silent=True)
+            await ctx.message.delete()
             return
         
-        
+        old_language = await bot.db.fetchval('SELECT translate_to FROM users WHERE user_id = $1', ctx.author.id)
+        old_language = langcodes.get(old_language)
         await bot.db.execute('UPDATE users SET translate_to = $2 WHERE user_id = $1', ctx.author.id, language_code)
         
+        embed = discord.Embed(
+            title="Language Preference Updated!",
+            description=f'**{old_language.display_name()}** -> **{new_language.display_name()}**',
+            color=discord.Color.green(), 
+        )
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar.url)
         
-        await ctx.reply(f'Changed your language to {lang_obj.display_name()}', ephemeral=True)
+        await ctx.reply(embed=embed, delete_after=60.0, ephemeral=True, silent=True)
+        await ctx.message.delete()
 
 @app_commands.context_menu(name='Translate')
+@ensure_user_is_in_database_interaction()
 async def translate_message_context_menu(interaction: discord.Interaction, message: discord.Message):
-    
     translate_to = await bot.db.fetchval('SELECT translate_to FROM users WHERE user_id = $1', interaction.user.id)
     translate_to = langcodes.standardize_tag(translate_to)
     
-    embed = discord.Embed(color=discord.Color.red(), description=translator.translate(message.content, dest=translate_to).text)
+    translated_text = translator.translate(text=message.content, dest=translate_to, src='auto')
     
-    await interaction.response.send_message(embed=embed, ephemeral=True)
+    embed = discord.Embed(
+        color=discord.Color.red(), 
+        title=f'Translating from {langcodes.get(translated_text.src).display_name()} to {langcodes.get(translated_text.dest).display_name()}', 
+        description=f'*{translated_text.text}*', 
+        timestamp=message.created_at, 
+    )
+    embed.set_footer(text=message.author.display_name, icon_url=message.author.display_avatar.url)
+    
+    await interaction.response.send_message(embed=embed, ephemeral=True, silent=True, delete_after=60.0)
 
 bot.tree.add_command(translate_message_context_menu)
 
--- utilities.py
+++ utilities.py
@@ -1,15 +1,22 @@
+import discord
 from discord.ext import commands
+from discord import app_commands
 
 def ensure_user_is_in_database():
     async def predicate(ctx):
-        # Check if the user exists in the database
         user = await ctx.bot.db.fetchrow('SELECT 1 FROM users WHERE user_id = $1', ctx.author.id)
-
-        # If the user doesn't exist, insert them into the database
         if not user:
             await ctx.bot.db.execute('INSERT INTO users (user_id) VALUES ($1)', ctx.author.id)
-
-        # Continue with the command execution
         return True
 
     return commands.check(predicate)
+
+def ensure_user_is_in_database_interaction():
+    async def predicate(interaction: discord.Interaction):
+        bot = interaction.client
+        user = await bot.db.fetchrow('SELECT 1 FROM users WHERE user_id = $1', interaction.user.id)
+        if not user:
+            await bot.db.execute('INSERT INTO users (user_id) VALUES ($1)', interaction.user.id)
+        return True
+
+    return app_commands.check(predicate)
