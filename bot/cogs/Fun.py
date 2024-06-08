68be8565e40154acd557d10504a421cf1d8e8c08
router97 <router97.dev@gmail.com>
2024-06-08 13:51:37 +0300

    Added `Faker` library. Fixed `fog` command. **Reformatted** Fun cog. **Optimized** Fun cog. Added more **documentation** to Fun cog.

--- bot/cogs/Fun.py
+++ bot/cogs/Fun.py
@@ -2,79 +2,114 @@
 Fun
 ===
 
-This cog module contains miscellaneous commands, mostly without actual purpose.
+This cog module contains miscellaneous, non-essential commands.
 """
 
 import datetime
 import time
-from random import randint, choice
+from random import choice
 
 import discord
 from discord.ext import commands
+from faker import Faker
+
+faker = Faker()
 
 
 class Fun(commands.Cog):
-    """Miscellaneous commands"""
-    
-    emojis_num = ('1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟')
-    fakt_map = {
-        'a': 'ф', 'b': 'и', 'c': 'с', 'd': 'в', 'e': 'у', 'f': 'а', 'g': 'п', 'h': 'р',
-        'i': 'ш', 'j': 'о', 'k': 'л', 'l': 'д', 'm': 'ь', 'n': 'т', 'o': 'щ', 'p': 'з',
-        'q': 'й', 'r': 'к', 's': 'ы', 't': 'е', 'u': 'г', 'v': 'м', 'w': 'ц', 'x': 'ч',
-        'y': 'н', 'z': 'я', ',': 'б', '.': 'ю', '[': 'х', ']': 'ъ', "'": 'э', '`': 'ё',
-        ' ': ' '
-    }
-    random_replies = [
-        "I'm good", 
-        "KYS = Keep Yourself Safe", 
-        "Nah", 
-        "I'll pass on that one", 
-        "No, thanks", 
-        "/me Лицом к стене! 1... 2... 3... Стреляю!"
+    """
+    Miscellaneous commands.
+
+    This class contains non-essential random commands.
+    """
+    RANDOM_REPLIES: list[str] = [
+        "I'm good",
+        "KYS = Keep Yourself Safe",
+        "Nah",
+        "I'll pass on that one",
+        "No, thanks",
+        "/me Лицом к стене! 1... 2... 3... Стреляю!",
     ]
-    
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-    
-    @commands.hybrid_command(name="ip", description="Generate a random IP address")
-    async def ip(self, ctx: commands.Context):
-        """Generate a random IP address"""
-        await ctx.reply('.'.join(str(randint(0,255)) for _ in range(4)))
-    
-    @commands.hybrid_command(name="fog", description="the fog is coming.")
-    async def fog(self, ctx: commands.Context):
-        """The fog is coming"""
-        current_time = time.time()
-        fog_time = datetime.datetime(2026, 4, 7).timestamp()
-        time_difference = fog_time - current_time
-        remaining_time = datetime.timedelta(seconds=time_difference)
-        await ctx.reply(f'{remaining_time.days} days {round(remaining_time.seconds / 3600)} hours remaining...')
-    
-    @commands.hybrid_command(name="kys", description="Generate a random reply")
-    async def kys(self, ctx: commands.Context):
-        """Generate a random reply"""
-        await ctx.reply(choice(self.random_replies))
-    
+    FOG_ARRIVAL_TIMESTAMP: float = datetime.datetime(2026, 4, 7).timestamp()
+
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot: commands.Bot = bot
+
+    @commands.hybrid_command(name="ip", description="Generate a random IPv4 address.")
+    async def ip(self, ctx: commands.Context) -> None:
+        """
+        Replies with a random **IPv4** address generated using **Faker**.
+
+        :param ctx: The context in which the command is invoked.
+        :type ctx: commands.Context
+        :return: None. The function only sends a Discord message, doesn't return anything in the code.
+        :rtype: None
+        """
+        await ctx.reply(faker.ipv4())
+
+    @commands.hybrid_command(name="fog", description="The fog is coming...")
+    async def fog(self, ctx: commands.Context) -> None:
+        """
+        Replies with how much time is left until the **fog** comes.
+
+        :param ctx: The context in which the command is invoked.
+        :type ctx: commands.Context
+        :return: The function only sends a Discord message, doesn't return anything in the code.
+        :rtype: None
+        """
+        current_time: float = time.time()
+        time_difference: float = self.FOG_ARRIVAL_TIMESTAMP - current_time
+
+        if time_difference <= 0:
+            await ctx.reply('The fog is here...')
+
+        days, remainder = divmod(time_difference, 24 * 3600)
+        hours, remainder = divmod(remainder, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        remaining_time_string: str = (f'{days:.0f} days, '
+                                      f'{hours:.0f} hours, '
+                                      f'{minutes:.0f} minutes, '
+                                      f'{seconds:.0f} seconds '
+                                      f'remaining...')
+        await ctx.reply(remaining_time_string)
+
+    @commands.hybrid_command(name="kys", description="Kys.")
+    async def kys(self, ctx: commands.Context) -> None:
+        """
+        Replies with a random string from ``self.RANDOM_REPLIES``.
+
+        :param ctx: The context in which the command is invoked.
+        :type ctx: commands.Context
+        :return: The function only sends a Discord message, doesn't return anything in the code.
+        :rtype: None
+        """
+        await ctx.reply(choice(self.RANDOM_REPLIES))
+
     @commands.command(name='do', description='Change the presence of the bot.')
-    async def do(self, ctx: commands.Context, *, activity: str):
+    async def do(self, ctx: commands.Context, *, name: str) -> None:
         """
-        # Example:
-            **User:** ".do Team Fortress 2"
-            *Bot's presence became: "Playing Team Fortress 2"*
+        Changes the bot's presence.
+
+        :param ctx: The context in which the command is invoked.
+        :type ctx: commands.Context
+        :param name: The text, which would be set as the bot's activity.
+        :type name: str
+        :return: The function only sends a Discord message, doesn't return anything in the code.
+        :rtype: None
         """
-        
-        # Create Activity object
-        new_presence = discord.Activity(type=discord.ActivityType.playing, name=activity)
-        
-        # Change the presence
+        new_presence = discord.Activity(type=discord.ActivityType.playing, name=name)
         await self.bot.change_presence(activity=new_presence, status=discord.Status.do_not_disturb)
-        
-        # Acknowledge the action
-        await ctx.reply('changed the presence!', ephemeral=True)
-    
-    async def cog_command_error(self, ctx: commands.Context, error):
+        await ctx.reply(
+            content=f"Changed the bot's presence to `{name}`.",
+            delete_after=60.0,
+            allowed_mentions=discord.AllowedMentions.none(),
+            ephemeral=True,
+            silent=True,
+        )
+
+    async def cog_command_error(self, ctx: commands.Context, error: Exception) -> None:
         await ctx.send_help(ctx.command)
-        print(repr(error))
 
 
 def setup(bot: commands.Bot):
